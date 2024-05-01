90c7543bc0548ba369b7808340317b540188028a
Gloria Hornero <snootchie.boochies@gmail.com>
2024-05-01 18:41:32 +0200

    seed refactor

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,22 @@
+import { PrismaClientValidationError } from '@prisma/client/runtime/library';
 import { db } from './db';
+import { type Booking, type Dog, type Owner, type Room } from "@prisma/client";
 
-const createOwner = async (fullName: string, address: string) => {
+const createOwner = async (fullName: string, email:string, address: string): Promise<Owner> => {
+    console.log(`Creating owner with fullName: ${fullName}`);
+
     return await db.owner.create({
             data: {
                 fullName,
+                email,
                 address
             }
         })
 };
 
-const createDog = async (name: string, breed: string, ownerId: number) => {
+const createDog = async (name: string, breed: string, ownerId: number): Promise<Dog> => {
+    console.log(`Creating dog with name: ${name}`);
+
     return await db.dog.create({
             data: {
                 name,
@@ -19,7 +26,9 @@ const createDog = async (name: string, breed: string, ownerId: number) => {
         })
 };
 
-const createRoom = async (name: string, roomNumber: number, size: number) => {
+const createRoom = async (name: string, roomNumber: number, size: number): Promise<Room> => {
+    console.log(`Creating room with name: ${name}`);
+
     return await db.room.create({
         data: {
             name,
@@ -29,7 +38,9 @@ const createRoom = async (name: string, roomNumber: number, size: number) => {
     })
 };
 
-const createBooking = async (entryDate: string, exitDate: string, price: number, dogId: number, roomId: number) => {
+const createBooking = async (entryDate: string, exitDate: string, price: number, dogId: number, roomId: number): Promise<Booking> => {
+    console.log(`Creating booking for the dogId: ${dogId}`);
+
     return await db.booking.create({
         data: {
             entryDate,
@@ -47,24 +58,34 @@ const roomLog = (name: string, roomId: number) => console.log(`Created room with
 const bookingLog = (bookingId: number) => console.log(`Created boowking with id: ${bookingId}`);
 
 
-const gloria = await createOwner('Gloria Hornero', 'Sant Pere de Ribes');
-ownerLog(gloria.fullName, gloria.ownerId);
-const ariadna = await createOwner('Ariadna F', 'Sant Pere de Ribes');
-ownerLog(ariadna.fullName, ariadna.ownerId);
+const prismaCatchErrors = async (myMethod: any) => {
+    try {
+        return await myMethod
+    } catch (e) {
+        const prismaError = e as PrismaClientValidationError;
+        console.log(prismaError.message);
+        process.exit(1);
+    }
+}
 
-const xoco = await createDog('Xoco', 'Mix', gloria.ownerId);
-dogLog(xoco.name, xoco.dogId);
-const canino = await createDog('Canino', 'Tulear Cotton', ariadna.ownerId);
-dogLog(canino.name, canino.dogId);
-const avatar = await createDog('Avatar', 'Tulear Cotton', ariadna.ownerId);
-dogLog(avatar.name, avatar.dogId);
+const gloria = await prismaCatchErrors(createOwner('Gloria Hornero', 'gloria@test.com', 'Sant Pere de Ribes'));
+if (gloria)  ownerLog(gloria.fullName, gloria.ownerId);
+const ariadna = await prismaCatchErrors(createOwner('Ariadna F', 'ariadna@test.com', 'Sant Pere de Ribes'));
+if (ariadna) ownerLog(ariadna.fullName, ariadna.ownerId);
 
-const jungleRoom = await createRoom('The Jungle', 700, 50);
-roomLog(jungleRoom.name, jungleRoom.roomId);
-const gardenRoom = await createRoom('The Garden', 717, 30);
-roomLog(gardenRoom.name, gardenRoom.roomId);
-const dreamRoom = await createRoom('The Dream', 720, 60);
-roomLog(dreamRoom.name, dreamRoom.roomId);
+const xoco = await prismaCatchErrors(createDog('Xoco', 'Mix', gloria.ownerId));
+if (xoco) dogLog(xoco.name, xoco.dogId);
+const canino = await prismaCatchErrors(createDog('Canino', 'Tulear Cotton', ariadna.ownerId));
+if (canino) dogLog(canino.name, canino.dogId);
+const avatar = await prismaCatchErrors(createDog('Avatar', 'Tulear Cotton', ariadna.ownerId));
+if (avatar) dogLog(avatar.name, avatar.dogId);
 
-const caninoBooking = await createBooking('2024-04-28T08:00:00Z', '2024-05-03T17:00:00Z', 300, canino.dogId, dreamRoom.roomId);
-bookingLog(caninoBooking.bookId);
+const jungleRoom = await prismaCatchErrors(createRoom('The Jungle', 700, 50));
+if (jungleRoom) roomLog(jungleRoom.name, jungleRoom.roomId);
+const gardenRoom = await prismaCatchErrors(createRoom('The Garden', 717, 30));
+if (gardenRoom) roomLog(gardenRoom.name, gardenRoom.roomId);
+const dreamRoom = await prismaCatchErrors(createRoom('The Dream', 720, 60));
+if(dreamRoom) roomLog(dreamRoom.name, dreamRoom.roomId);
+
+const caninoBooking = await prismaCatchErrors(createBooking('2024-04-28T08:00:00Z', '2024-05-03T17:00:00Z', 300, canino.dogId, dreamRoom.roomId));
+if (caninoBooking) bookingLog(caninoBooking.bookId);
